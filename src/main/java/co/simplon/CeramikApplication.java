0cc556ed8351e88d024f04aaac8f35d20191ec9b
Llaurentt <24laurent65@gmail.com>
2017-04-26 15:31:07 +0200

    mon 1er commit

---
+++ src/main/java/co/simplon/CeramikApplication.java
@@ -0,0 +1,12 @@
+package co.simplon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeramikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeramikApplication.class, args);
+	}
+}
