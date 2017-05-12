56239c77c8c40ab14c583524d971e5f882a024a5
Llaurentt <24laurent65@gmail.com>
2017-05-12 09:45:16 +0200

    -

--- src/main/java/co/simplon/domain/Adresse.java
+++ src/main/java/co/simplon/domain/Adresse.java
@@ -1,13 +1,16 @@
 package co.simplon.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+
 
 
 @Entity
--- src/main/java/co/simplon/service/UtilisateurServiceImpl.java
+++ src/main/java/co/simplon/service/UtilisateurServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.simplon.dao.AdresseDao;
 import co.simplon.dao.RoleDao;
@@ -30,34 +31,47 @@ public class UtilisateurServiceImpl implements UtilisateurService{
 	}
 
 	@Override
+	@Transactional
 	public List<Utilisateur> listUtilisateur() {
 		List<Utilisateur> resultat = utilisateurDao.findAll();
 		List<Utilisateur> result = new ArrayList<>();
+		
 		for (Utilisateur u : resultat){
+//			Utilisateur utilisateur = utilisateurDao.findOne(u.getId());
+			Utilisateur utilisateur = new Utilisateur();
+				utilisateur.setId(u.getId());
+				utilisateur.setUsername(u.getUsername());
+				utilisateur.setPassword(u.getPassword());
+				utilisateur.setNom(u.getNom());
+				utilisateur.setPrenom(u.getPrenom());
+				utilisateur.setEmail(u.getEmail());
+				utilisateur.setTelephone(u.getTelephone());
+				utilisateur.setAvatar_utilisateur(u.getAvatar_utilisateur());
+				utilisateur.setActif(u.getActif());
 			
-			Utilisateur utilisateur = utilisateurDao.findOne(u.getId());
-//				utilisateur.setId(u.getId());
-//				utilisateur.setUsername(u.getUsername());
-//				utilisateur.setPassword(u.getPassword());
-//				utilisateur.setNom(u.getNom());
-//				utilisateur.setPrenom(u.getPrenom());
-//				utilisateur.setEmail(u.getEmail());
-//				utilisateur.setTelephone(u.getTelephone());
-//				utilisateur.setAvatar_utilisateur(u.getAvatar_utilisateur());
-//				utilisateur.setActif(u.getActif());
-//			
-			Role role = roleDao.findOne(u.getRole().getId());
-//			 	role.setId(u.getRole().getId());
-//		 		role.setNom(u.getRole().getNom());
+//			Role role = roleDao.findOne(u.getRole().getId());
+			Role role = new Role();
+			 	role.setId(u.getRole().getId());
+		 		role.setNom(u.getRole().getNom());
 		 		utilisateur.setRole(role);
-//		 	adresseDao.findOne(u.getAdresse().getId());
-			Adresse adresse = adresseDao.findOne(u.getAdresse().getId());
-//				adresse.setId(u.getAdresse().getId());
-//				adresse.setAdresse(u.getAdresse().getAdresse());
-//				adresse.setCode_postal(u.getAdresse().getCode_postal());
-//				adresse.setVille(u.getAdresse().getVille());
-//				adresse.setPays(u.getAdresse().getPays());
-				utilisateur.setAdresse(adresse);
+		 	
+		 	Adresse adresse = new Adresse();
+//		 	Adresse adresse = adresseDao.findOne(u.getAdresse().getId());
+		 		if (u.getAdresse().getId().equals(null)){
+					adresse.setId(null);
+					adresse.setAdresse("");
+					adresse.setCode_postal("");
+					adresse.setVille("");
+					adresse.setPays("");
+					utilisateur.setAdresse(adresse);
+		 		} else {
+		 			adresse.setId(u.getAdresse().getId());
+					adresse.setAdresse(u.getAdresse().getAdresse());
+					adresse.setCode_postal(u.getAdresse().getCode_postal());
+					adresse.setVille(u.getAdresse().getVille());
+					adresse.setPays(u.getAdresse().getPays());
+					utilisateur.setAdresse(adresse);
+		 		}
 				
 				result.add(utilisateur);
 		}
